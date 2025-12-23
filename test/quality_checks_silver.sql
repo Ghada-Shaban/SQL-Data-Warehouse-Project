bd8fd638ae2d08c36bfcf2fd3d7fa5f0500b2392
Ghada-Shaban <ghadaashaban2003@gmail.com>
2025-12-23 11:08:17 +0200

    Update quality_checks_silver.sql

--- test/quality_checks_silver.sql
+++ test/quality_checks_silver.sql
@@ -107,3 +107,46 @@ SELECT
 FROM silver.crm_prd_info
 WHERE prd_end_dt < prd_start_dt;
 ---------------------------------------
+
+-- ====================================================================
+-- Checking 'silver.crm_sales_details'
+-- ====================================================================
+
+-- Check For Invalid Dates
+-- Expectation : No Result
+
+SELECT 
+	NULLIF(sls_order_dt,0) AS sls_order_dt
+FROM bronze.crm_sales_details
+WHERE sls_order_dt <= 0
+OR sls_order_dt > 20500101
+OR sls_order_dt < 19000101
+OR LEN(sls_order_dt) != 8;
+---------------------------------------
+
+-- Check For Invalid Date Orders
+-- Expectation : No Result
+
+SELECT 
+	*
+FROM silver.crm_sales_details
+WHERE sls_order_dt > sls_ship_dt OR sls_order_dt > sls_due_dt;
+---------------------------------------
+
+-- Check Data Consistency
+-- Sales = Quantity * Price  (Must not be null ,0 or negative)
+-- Expectation : No Result
+SELECT DISTINCT
+	sls_sales,
+    sls_quantity,
+    sls_price
+FROM silver.crm_sales_details
+WHERE sls_sales <= 0 OR sls_quantity <= 0 OR sls_price <= 0
+OR sls_sales IS NULL OR sls_quantity IS NULL OR sls_price IS NULL
+OR sls_sales != sls_price * sls_quantity
+ORDER BY sls_sales,
+    sls_quantity,
+    sls_price;
+
+SELECT * FROM silver.crm_sales_details;
+---------------------------------------
